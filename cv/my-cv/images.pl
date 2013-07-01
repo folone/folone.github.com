9059cf0922eadb734ad30558ba07a00189745034
folone <folone@gmail.com>
2013-07-01 11:43:59 +0200

    Description.

--- cv/my-cv/images.pl
+++ cv/my-cv/images.pl
@@ -2,6 +2,12 @@
 # Associate images original text with physical files.
 
 
+$key = q/bullet;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="14" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$\bullet$">|; 
+
 $key = q/to;MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="24" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
@@ -13,12 +19,6 @@ $cached_env_img{$key} = q|<IMG
  WIDTH="15" HEIGHT="20" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img1.png"
  ALT="$\lambda$">|; 
-
-$key = q/bullet;MSF=1.6;LFS=12;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="14" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="$\bullet$">|; 
 
 1;
 
