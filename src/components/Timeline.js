b3bbf6e92b66e1fba73c2a31140cb9acdab4a8b4
George Leontiev <gleontiev@twitter.com>
2020-08-13 15:40:47 +0000

    .

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,23 +8,19 @@ const CONTENT = [
   },
   {
     date: '2014/02/01 – 2017/11/15',
-    content: <div><p>I'm an engineer living in Berlin <span role='img'>🇩🇪</span>.</p><p>I <a href="https://www.linkedin.com/in/yukifartlek">work</a> for SoundCloud, and I'm mostly into <a href="https://www.strava.com/athletes/yukifartlek">running</a>.</p></div>
-  },
-  {
-    date: '2015/01/01',
-    content: <div>Today I went for my first ever run, not knowing how much this will completely change my life: <a href="https://www.strava.com/activities/235204211">https://www.strava.com/activities/235204211</a></div>
+    content: <div><p>I'm an engineer living in Berlin <span role='img'>🇩🇪</span>.</p><p>On the first day of 2015 I <a href="https://www.strava.com/activities/235204211">went for my first run</a>, not realizing how much it will change my life. That same year I've finished <a href="https://www.strava.com/activities/433168211">my first marathon</a>, and in the next three years went on to <a href="https://www.strava.com/activities/1199206700">steadily improve my time</a>.</p><p>I <a href="https://www.linkedin.com/in/yukifartlek">work</a> for SoundCloud, and I'm mostly into <a href="https://www.strava.com/athletes/yukifartlek">running</a>.</p></div>
   },
   {
     date: '2017/11/15 – 2020/06/07',
-    content: <div><p>I'm an engineer living in London <span role='img'>🇬🇧</span>.</p><p>I <a href="https://www.linkedin.com/in/yukifartlek">work</a> for Twitter, and I'm into <a href="https://www.strava.com/athletes/yukifartlek">running</a> and <a href="https://www.goodreads.com/user/show/6328696-george-leontiev">reading</a>.</p></div>
+    content: <div><p>I'm an engineer living in London <span role='img'>🇬🇧</span>.</p><p>In 2018 I raced three World Majors Marathons in a span of less than two months: <a href="https://www.strava.com/activities/1844986783/">Berlin</a>, <a href="https://www.strava.com/activities/1890384566">Chicago</a>, <a href="https://www.strava.com/activities/1946467133">New York</a>.</p><p>I <a href="https://www.linkedin.com/in/yukifartlek">work</a> for Twitter, and I'm into <a href="https://www.strava.com/athletes/yukifartlek">running</a> and <a href="https://www.goodreads.com/user/show/6328696-george-leontiev">reading</a>.</p></div>
   },
   {
     date: '2020/06/07 – 2020/08/23',
-    content: <div><p>I've fled to Reykjavik <span role='img'>🇮🇸</span> for the summer to escape COVID-19.</p><p>While here, I've <a href="https://twitter.com/yukifartlek/status/1281563610758742017">cycled around Iceland</a> and <a href="https://twitter.com/yukifartlek/status/1284361299728764929">ran my first ultramarathon</a>.</p><p>Speaking of crazy things to do in Iceland, last year I became the fastest non-Icelander (and the first Ukrainian) <a href="https://twitter.com/yukifartlek/status/1126594523256090624">Landvættur</a>.</p><p>I <a href="https://www.linkedin.com/in/yukifartlek">work</a> for Twitter, and I'm into adventures – happening <a href="https://www.strava.com/athletes/yukifartlek">in the outside world</a> as well as <a href="https://www.goodreads.com/user/show/6328696-george-leontiev">in my head</a>.</p></div>
+    content: <div><p>I've fled to Reykjavik <span role='img'>🇮🇸</span> for the summer to escape COVID-19.</p><p>While here, I've <a href="https://twitter.com/yukifartlek/status/1281563610758742017">cycled around Iceland</a> and <a href="https://twitter.com/yukifartlek/status/1284361299728764929">ran my first ultramarathon</a>.</p><p>Speaking of crazy things to do in Iceland, last year I became the fastest non-Icelander (and the first Ukrainian) <a href="https://twitter.com/yukifartlek/status/1126594523256090624">Landvættur</a>.</p><p>I <a href="https://www.linkedin.com/in/yukifartlek">work</a> for Twitter, and I'm into adventures – both happening <a href="https://www.strava.com/athletes/yukifartlek">in the outside world</a> as well as <a href="https://www.goodreads.com/user/show/6328696-george-leontiev">in my head</a>.</p></div>
   },
   {
     date: '2020/08/23',
-    content: <div><p>I'm an engineer living in Edinburgh <span role='img'>🏴󠁧󠁢󠁳󠁣󠁴󠁿</span>.</p><p>I <a href="https://www.linkedin.com/in/yukifartlek">work</a> for Twitter, and I'm into <a href="https://www.strava.com/athletes/yukifartlek">running</a> and <a href="https://www.goodreads.com/user/show/6328696-george-leontiev">reading</a>.</p></div>
+    content: <div><p>I'm an engineer living in Edinburgh <span role='img'>🏴󠁧󠁢󠁳󠁣󠁴󠁿</span>.</p><p>I <a href="https://www.linkedin.com/in/yukifartlek">work</a> for Twitter, and I'm into adventures – both happening <a href="https://www.strava.com/athletes/yukifartlek">in the outside world</a> as well as <a href="https://www.goodreads.com/user/show/6328696-george-leontiev">in my head</a>.</p></div>
   },
 ];
 
