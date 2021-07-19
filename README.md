db60a346a48ec1675750be5eef8f4598d88d37ee
George Leontiev <gleontiev@twitter.com>
2021-07-19 15:33:33 +0100

    .

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 * `npm install --global gatsby-cli` && `npm install gh-pages --save-dev` – install scripts
 * `npm install` – install dependencies
 * `gatsby develop` – run locally
-* `npm deploy` – deploy to prod
+* `npm run deploy` – deploy to prod
 * `git push origin source` – don't forget to push the underlying changes
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
 import config from '../../config';
-import loadable from '@loadable/component'
 
 export default function Header() {
-  const cities = combine(config.cities);
-  const jobs = combine(config.work);
-  const Timeline = loadable(() => import('../components/Timeline'))
   return (
     <header id="header">
-      <h1><span role="img">👋</span> My name is {config.authorName} and</h1>
-      <Timeline />
+      <h1><span role="img">👋</span> My name is {config.authorName}</h1>
       <nav>
         <ul>
           {config.socialLinks.map(social => {
@@ -27,12 +22,4 @@ export default function Header() {
       </nav>
     </header>
   );
-}
-
-function combine(arr) {
-  const stuff = arr.map((elem, index) =>
-    (index === arr.length - 1) ? <span> {elem}</span> : <s> {elem}</s>
-  );
-
-  return stuff;
 }
